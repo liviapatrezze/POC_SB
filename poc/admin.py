217f67040fb330676ae21a49f80fac52f25d5f8c
Livia <liviapatrezze@gmail.com>
2026-03-30 22:46:54 -0300

    adicionado Docker compose e ajustado o layout

--- poc/admin.py
+++ poc/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
-
-from django.contrib import admin
 from .models import Sprint, Squad, SquadMember, Task, TaskImage
 
 admin.site.register(Sprint)
